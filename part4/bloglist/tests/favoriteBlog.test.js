b5d9a35195b5cb9c8a8ac33e973ed07356588493
[KirstiH] <[kirsti.haro@gmail.com]>
2025-03-30 14:54:25 +0300

    4.5 Bloglist

---
+++ part4/bloglist/tests/favoriteBlog.test.js
@@ -0,0 +1,48 @@
+const { test, describe } = require('node:test')
+const assert = require('node:assert');
+
+const listHelper = require('../utils/list_helper.js')
+
+describe('favorite blog', () => {
+    const blogs = [
+        {
+          title: "React patterns",
+          author: "Michael Chan",
+          likes: 7,
+        },
+        {
+          title: "Go To Statement Considered Harmful",
+          author: "Edsger W. Dijkstra",
+          likes: 5,
+        },
+        {
+          title: "Canonical string reduction",
+          author: "Edsger W. Dijkstra",
+          likes: 12,
+        },
+        {
+          title: "First class tests",
+          author: "Robert C. Martin",
+          likes: 10,
+        },
+        {
+          title: "TDD harms architecture",
+          author: "Robert C. Martin",
+          likes: 0,
+        },
+        {
+          title: "Type wars",
+          author: "Robert C. Martin",
+          likes: 2,
+        }  
+    ]
+  
+    test('returns information of favorite blog', () => {
+      const result = listHelper.favoriteBlog(blogs)
+      assert.deepStrictEqual(result, {
+        title: "Canonical string reduction",
+        author: "Edsger W. Dijkstra",
+        likes: 12
+      })
+    })
+})
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -5,8 +5,20 @@ const dummy = (blogs) => {
 const totalLikes = (blogs) => {
     return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
+
+const favoriteBlog = (blogs) => {
+
+    var favoriteBlog = blogs[0]
+    for (let i = 1; i < blogs.length; i++) {
+        if (blogs[i].likes > favoriteBlog.likes) {
+            favoriteBlog = blogs[i]
+        }
+    }
+    return favoriteBlog
+}
   
 module.exports = {
     dummy,
     totalLikes,
+    favoriteBlog,
 }
