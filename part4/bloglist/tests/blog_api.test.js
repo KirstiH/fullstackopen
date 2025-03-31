1adf261d007b484045b90c3cb64102eb7843a5a9
[KirstiH] <[kirsti.haro@gmail.com]>
2025-03-31 19:39:38 +0300

    4.8 bloglist tests

---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,47 @@
+const { test, after, beforeEach } = require('node:test')
+const Blog = require('../models/blog')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const assert = require('node:assert')
+const api = supertest(app)
+
+const initialBlogs = [
+  {
+    'title': 'Coding!',
+    'author': 'Adam',
+    'url': 'https://code.com',
+    'likes': 105
+  },
+  {
+    'title': 'HTML is easy!',
+    'author': 'Friedrich',
+    'url': 'https://html.com',
+    'likes': 11
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('there are two blogs', async () => {
+  const response = await api.get('/api/blogs')
+
+  assert.strictEqual(response.body.length, 2)
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
