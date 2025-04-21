3c47c575329ed08ea430de37a7ab6ed332a6dc92
[KirstiH] <[kirsti.haro@gmail.com]>
2025-04-21 19:24:59 +0300

    5.17 bloglist

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -144,6 +144,7 @@ const App = () => {
           <div>
           username
             <input
+              data-testid='username'
               type="text"
               value={username}
               name="Username"
@@ -153,6 +154,7 @@ const App = () => {
           <div>
           password
             <input
+              data-testid='password'
               type="password"
               value={password}
               name="Password"
---
+++ part5/playwright/tests/blog_app.spec.js
@@ -0,0 +1,31 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+
+describe('Blog app', () => {
+    beforeEach(async ({ page }) => {
+      await page.goto('http://localhost:5173')
+    })
+  
+    test('Login form is shown', async ({ page }) => {
+        const locator = await page.getByText('Log in to application')
+        await expect(locator).toBeVisible()
+        await page.getByTestId('username').fill('Kirsti')
+        await page.getByTestId('password').fill('salainen')
+        await expect(page.getByRole('button', { name: 'login' })).toBeVisible()
+    })
+
+    test('Login with correct credentials', async ({ page }) => {
+        await page.getByRole('textbox').first().fill('Kirsti')
+        await page.getByRole('textbox').last().fill('salainen')
+        await page.getByRole('button', { name: 'login' }).click()
+
+        await expect(page.getByText('Kirsti logged-in')).toBeVisible()
+    })
+
+    test('Login with false credentials', async ({ page }) => {
+        await page.getByRole('textbox').first().fill('Kirsti')
+        await page.getByRole('textbox').last().fill('salainen')
+        await page.getByRole('button', { name: 'login' }).click()
+
+        await expect(page.getByText('Kirsti logged-in')).toBeVisible()
+    })
+})
