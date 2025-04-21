a9698161c71422bcf169e02ba533a399f216d5b5
[KirstiH] <[kirsti.haro@gmail.com]>
2025-04-21 19:29:00 +0300

    5.17 bloglist

--- part5/playwright/tests/blog_app.spec.js
+++ part5/playwright/tests/blog_app.spec.js
@@ -8,20 +8,20 @@ describe('Blog app', () => {
     test('Login form is shown', async ({ page }) => {
         const locator = await page.getByText('Log in to application')
         await expect(locator).toBeVisible()
-        await page.getByTestId('username').fill('Kirsti')
-        await page.getByTestId('password').fill('salainen')
+        await expect(page.getByText('username')).toBeVisible()
+        await expect(page.getByText('password')).toBeVisible()
         await expect(page.getByRole('button', { name: 'login' })).toBeVisible()
     })
 
     test('Login with correct credentials', async ({ page }) => {
-        await page.getByRole('textbox').first().fill('Kirsti')
-        await page.getByRole('textbox').last().fill('salainen')
+        await page.getByTestId('username').fill('Kirsti')
+        await page.getByTestId('password').fill('salainen')
         await page.getByRole('button', { name: 'login' }).click()
 
         await expect(page.getByText('Kirsti logged-in')).toBeVisible()
     })
 
-    test('Login with false credentials', async ({ page }) => {
+    test('Login fails with false credentials', async ({ page }) => {
         await page.getByRole('textbox').first().fill('Kirsti')
         await page.getByRole('textbox').last().fill('salainen')
         await page.getByRole('button', { name: 'login' }).click()
