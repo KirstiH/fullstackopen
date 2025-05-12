96b078faf084cebbd2f6bc735b3234dee2fd9785
[KirstiH] <[kirsti.haro@gmail.com]>
2025-05-12 19:37:38 +0300

    6.23 query anecdotes

--- part6/query_anecdote/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/query_anecdote/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -5,23 +5,29 @@ import { useNotificationDispatch } from '../notificationContext'
 
 
 const AnecdoteForm = () => {
+  const notificationDispatch = useNotificationDispatch()
   const queryClient = useQueryClient()
   const newAnecdoteMutation = useMutation({ 
     mutationFn: createAnecdote, 
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+      notificationDispatch({ type: 'SET', payload: `you created anecdote '${newAnecdoteMutation.variables.content}'` })
+      setTimeout(() => {
+        notificationDispatch({ type: 'CLEAR' })
+      }, 5000)
     },
+    onError: () => {
+      queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+      notificationDispatch({ type: 'SET', payload: `anecdote must be at least 5 characters long` })
+      setTimeout(() => {
+        notificationDispatch({ type: 'CLEAR' })
+      }, 5000)
+    }
   })
-
-  const notificationDispatch = useNotificationDispatch()
 
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
-    notificationDispatch({ type: 'SET', payload: `you created anecdote '${content}'` })
-    setTimeout(() => {
-      notificationDispatch({ type: 'CLEAR' })
-    }, 5000)
     event.target.anecdote.value = ''
     newAnecdoteMutation.mutate({ content, votes: 0 })
 }
--- part6/query_anecdote/query-anecdotes/src/requests.jsx
+++ part6/query_anecdote/query-anecdotes/src/requests.jsx
@@ -5,8 +5,13 @@ const baseUrl = 'http://localhost:3001/anecdotes'
 export const getAnecdotes = () =>
   axios.get(baseUrl).then(res => res.data)
 
-export const createAnecdote = newAnecdote => 
-    axios.post(baseUrl, newAnecdote).then(res => res.data)
+export const createAnecdote = newAnecdote => {
+  if (newAnecdote.content.length < 5) {
+    throw new Error('anecdote must be at least 5 characters long')
+  }
+  axios.post(baseUrl, newAnecdote).then(res => res.data)
+}
+    
 
 export const updateAnecdote = anecdote => 
     axios.put(`${baseUrl}/${anecdote.id}`, anecdote).then(res => res.data)
