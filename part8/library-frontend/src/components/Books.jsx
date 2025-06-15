9db3a53576ccc60c5f96f477334813e11df4756e
[KirstiH] <[kirsti.haro@gmail.com]>
2025-06-15 15:17:31 +0300

    8.19

--- part8/library-frontend/src/components/Books.jsx
+++ part8/library-frontend/src/components/Books.jsx
@@ -1,12 +1,28 @@
+import { useState } from 'react'
+
 const Books = ({ books, show }) => {
+  const [selectedGenre, setSelectedGenre] = useState(null)
   if (!show) {
     return null
   }
 
+  const genres = []
+  books.forEach((book) => {
+    book.genres.forEach((genre) => {
+      if (!genres.includes(genre)) {
+        genres.push(genre)
+      }
+    })
+  })
+
+    const filteredBooks = selectedGenre
+    ? books.filter((book) => book.genres.includes(selectedGenre))
+    : books
+
   return (
     <div>
       <h2>books</h2>
-
+      <p hidden={!selectedGenre}>in genre <strong>{selectedGenre}</strong></p>
       <table>
         <tbody>
           <tr>
@@ -14,7 +30,7 @@ const Books = ({ books, show }) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
+          {filteredBooks.map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -23,6 +39,14 @@ const Books = ({ books, show }) => {
           ))}
         </tbody>
       </table>
+      <div>
+      {genres.map((a) => (
+        <button key={a} onClick={() => setSelectedGenre(a)}>
+          {a}
+        </button>
+      ))}
+      <button onClick={() => setSelectedGenre(null)}>all genres</button>
+      </div>
     </div>
   )
 }
