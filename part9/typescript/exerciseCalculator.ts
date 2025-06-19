2bf0ca39f0e1503a1d9a77d1c26e376a7a7c999a
[KirstiH] <[kirsti.haro@gmail.com]>
2025-06-19 19:19:58 +0300

    9.3 calculators

--- part9/typescript/exerciseCalculator.ts
+++ part9/typescript/exerciseCalculator.ts
@@ -1,19 +1,59 @@
-const exerciseCalculator = (dailyHours: number[], target: number): Object => {
+interface exerciceValues {
+  value1: number;
+  value2: number[];
+}
+
+const parseArguments2 = (args: string[]): exerciceValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+
+  let target = 0;
+  let numbers = [];
+
+  if (isNaN(Number(args[2]))) {
+    throw new Error('Provided values were not numbers!');
+  }
+  else {
+    target = Number(args[2]);
+  }
+
+  console.log('args length', args.length);
+  for (let i = 3; i < args.length; i++) {
+    console.log('i', i);
+    if (isNaN(Number(args[i]))) {
+      throw new Error('Provided values were not numbers!');
+    }
+    else {
+      numbers.push(Number(args[i]));
+    }
+  } 
+
+    console.log('target', target);
+    console.log('numbers', numbers);
+
+    return {
+        value1: target,
+        value2: numbers
+    }
+
+}
+
+
+const exerciseCalculator = (target : number, numbers: number[]): Object => {
     let hours = 0;
     let trainingDays = 0;
 
-    for (let i = 0; i < dailyHours.length; i++) {
-        hours += dailyHours[i];
-        if (dailyHours[i] > 0) {
+    for (let i = 0; i < numbers.length; i++) {
+        hours += numbers[i];
+        if (numbers[i] > 0) {
             trainingDays++;
         }
     }
 
-    let average = hours / dailyHours.length
+    let average = hours / numbers.length
 
     if (average < target - 1) {
         return {
-            periodLength: dailyHours.length,
+            periodLength: numbers.length,
             trainingDays: trainingDays,
             success: false,
             rating: 1,
@@ -24,7 +64,7 @@ const exerciseCalculator = (dailyHours: number[], target: number): Object => {
     }
     else if (average < target) {
         return {
-            periodLength: dailyHours.length,
+            periodLength: numbers.length,
             trainingDays: trainingDays,
             success: false,
             rating: 2,
@@ -35,7 +75,7 @@ const exerciseCalculator = (dailyHours: number[], target: number): Object => {
     }
     else {
         return {
-            periodLength: dailyHours.length,
+            periodLength: numbers.length,
             trainingDays: trainingDays,
             success: true,
             rating: 3,
@@ -48,7 +88,8 @@ const exerciseCalculator = (dailyHours: number[], target: number): Object => {
 
 
 try {
-    const result =exerciseCalculator([3, 0, 2, 4.5, 0, 3, 1], 3);
+    const { value1, value2 } = parseArguments2(process.argv);
+    const result = exerciseCalculator(value1, value2);
     console.log(result);
 } catch (error: unknown) {
     let errorMessage = 'Something went wrong: '
@@ -57,3 +98,15 @@ try {
     }
     console.log(errorMessage);
 }
+
+// try {
+//   const { value1, value2 } = parseArguments(process.argv);
+//   const result = calculateBmi(value1, value2);
+//   console.log(result);
+// } catch (error: unknown) {
+//   let errorMessage = 'Something bad happened.'
+//   if (error instanceof Error) {
+//     errorMessage += ' Error: ' + error.message;
+//   }
+//   console.log(errorMessage);
+// }
